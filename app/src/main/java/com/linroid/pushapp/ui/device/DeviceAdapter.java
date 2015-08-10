600288912f3bb806343806366c6f409245cc136a
linroid <linroid@gmail.com>
2015-08-10 23:46:41 +0800

    [optimize]完善设备列表Item内容

--- app/src/main/java/com/linroid/pushapp/ui/device/DeviceAdapter.java
+++ app/src/main/java/com/linroid/pushapp/ui/device/DeviceAdapter.java
@@ -1,31 +1,20 @@
 package com.linroid.pushapp.ui.device;
 
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
-import android.support.v7.widget.CardView;
+import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.Formatter;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.TouchDelegate;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.PopupMenu;
+import android.widget.Space;
 import android.widget.TextView;
 
 import com.linroid.pushapp.R;
 import com.linroid.pushapp.model.Device;
-import com.linroid.pushapp.model.Pack;
 import com.linroid.pushapp.ui.base.DataAdapter;
-import com.linroid.pushapp.util.AndroidUtil;
-import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import hugo.weaving.DebugLog;
 
 /**
  * Created by linroid on 7/20/15.
@@ -46,14 +35,42 @@ public class DeviceAdapter extends DataAdapter<Device, DeviceAdapter.DeviceHolde
 
     @Override
     public void onBindViewHolder(DeviceHolder holder, int i) {
+        Resources resources = holder.aliasTV.getResources();
+
         Device device = data.get(i);
         holder.aliasTV.setText(device.getAlias());
+        String screenText =resources.getString(R.string.txt_device_screen,
+                                                    device.getWidth(),
+                                                    device.getHeight(),
+                                                    device.getDpi());
+        String modelText = resources.getString(R.string.txt_device_model, device.getModel());
+        String systemText = resources.getString(R.string.txt_device_system, device.getOsName(), device.getSdkLevel());
+        String cpuTypeText = resources.getString(R.string.txt_device_cpu_type, device.getCpuType());
+        String memorySize = resources.getString(R.string.txt_device_memory_size,
+                                            Formatter.formatFileSize(holder.aliasTV.getContext(), device.getMemorySize()));
+
+        holder.modelTV.setText(modelText);
+        holder.systemTV.setText(systemText);
+        holder.cpuTypeTV.setText(cpuTypeText);
+        holder.screenTV.setText(screenText);
+        holder.memorySizeTV.setText(memorySize);
     }
 
-    class DeviceHolder extends RecyclerView.ViewHolder{
+    class DeviceHolder extends RecyclerView.ViewHolder {
 
-        @Bind(R.id.device_alias)
+        @Bind(R.id.alias_tv)
         TextView aliasTV;
+        @Bind(R.id.model_tv)
+        TextView modelTV;
+        @Bind(R.id.system_tv)
+        TextView systemTV;
+        @Bind(R.id.cpu_type_tv)
+        TextView cpuTypeTV;
+        @Bind(R.id.screen_tv)
+        TextView screenTV;
+        @Bind(R.id.memory_size_tv)
+        TextView memorySizeTV;
+
         public DeviceHolder(final View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
